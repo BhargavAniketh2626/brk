140ce2fdafc23a4253cdc77ba9f1678d9c100578
190030223-buduputi bhargav aniketh <190030223@kluniversity.in>
2021-05-25 12:28:34 +0530

    commit

--- BRK/settings.py
+++ BRK/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6i=$&c=r!7xdxi1hnrmv-9p=mlu%*l&l=9ne_civ1f*41aaru*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
