2c9c55881b67b5868703589b85a04b44cef77492
190030223-buduputi bhargav aniketh <190030223@kluniversity.in>
2021-05-25 10:34:00 +0530

    commit

--- BRK/settings.py
+++ BRK/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6i=$&c=r!7xdxi1hnrmv-9p=mlu%*l&l=9ne_civ1f*41aaru*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','brkfashions.herokuapp.com']
 
 
 # Application definition
