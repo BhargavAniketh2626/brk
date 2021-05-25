226a2877c75fd9697ed6555bff4d75d4ff279445
190030223-buduputi bhargav aniketh <190030223@kluniversity.in>
2021-05-25 10:53:47 +0530

    commit

--- BRK/settings.py
+++ BRK/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6i=$&c=r!7xdxi1hnrmv-9p=mlu%*l&l=9ne_civ1f*41aaru*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','brkfashions.herokuapp.com']
 
@@ -134,3 +135,4 @@ SESSION_ENGINE = "django.contrib.sessions.backends.db"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
